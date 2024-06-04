08bcf0113efd28192befa26e3c55e550a2598f87
Jona van der Vlist - 1075709 <j.vdv@outlook.com>
2024-06-04 16:42:18 +0200

    chore: Update page layout and content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,49 @@
-<script lang='ts'>
-	import Button from "$components/button/button.svelte";
-import Heading from "$components/heading/heading.svelte";
-import Wrapper from "$components/wrapper/wrapper.svelte";
-
-    
+<script lang="ts">
+	import Button from '$components/button/button.svelte';
+	import Carousel from '$components/carousel/carousel.svelte';
+	import Slide from '$components/carousel/slide.svelte';
+	import Heading from '$components/heading/heading.svelte';
+	import Wrapper from '$components/wrapper/wrapper.svelte';
 </script>
 
+<Wrapper width="wide">
+	<section class="section who-are-you">
+		<div class="even-columns">
+			<div class="flow">
+				<Heading level={2}>Who are you?</Heading>
 
-<Wrapper width='wide'>
-    <section class="section who-are-you">
-        <div class="even-columns">
-
-            <div class="flow">
-                <Heading level={2}>
-                    Who are you?
-                </Heading>
-                
-                <p class="fs-600" style="--max-length: 60ch">
-                    I'm Jona, a software engineer from the Netherlands. I'm very passionate about web development. 
-                    and usually stay up late to work on my projects.
-                    I have leanrt a lot of things in the past few years and it seems to never be enough. 
-                    I'm always looking for new things, new technologies and new ways to improve my skills. 
-                </p>
-                <div class="flex-group" style="--flex-gap: var(--size-4)">
-                    <Button variant="primary">
-                        Chech out my projects
-                    </Button> 
-                    <Button variant="secondary">
-                        Contact me
-                    </Button>
-                </div>
-            </div>
-    </section>
-    <section class="section highlighed-projects">
-        <div class="even-columns">
-            <div class="flow text-center">
-                <Heading level={2}>
-                    Highlighted projects
-                </Heading>
-                <p class="fs-600 mx-auto" style="--max-length: 60ch">
-                    Here are some of the projects I'm most proud of. 
-                    I've worked on a lot of projects in the past few years, but these are the ones that I think are the most interesting.
-                </p>
-            </div>
-        </div>
-    </section>
+				<p class="fs-600" style="--max-length: 60ch">
+					I'm Jona, a software engineer from the Netherlands. I'm very passionate about web
+					development. and usually stay up late to work on my projects. I have leanrt a lot of
+					things in the past few years and it seems to never be enough. I'm always looking for new
+					things, new technologies and new ways to improve my skills.
+				</p>
+				<div class="flex-group" style="--flex-gap: var(--size-4)">
+					<Button variant="primary">Chech out my projects</Button>
+					<Button variant="secondary">Contact me</Button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="section highlighed-projects">
+		<div class="even-columns">
+			<div class="flow text-center">
+				<Heading level={2}>Highlighted projects</Heading>
+				<p class="fs-600 mx-auto" style="--max-length: 60ch">
+					Here are some of the projects I'm most proud of. I've worked on a lot of projects in the
+					past few years, but these are the ones that I think are the most interesting.
+				</p>
+			</div>
+			<div class="">
+				<Carousel controls>
+					{#each [1, 2, 3, 4] as slideNumber}
+						<Slide>
+							<img src="https://picsum.photos/800/400" alt="placeholder" width="100%" />
+							<p class="slide-number">{slideNumber}</p>
+						</Slide>
+					{/each}
+				</Carousel>
+			</div>
+		</div>
+	</section>
 </Wrapper>
